012be7edbdd5af151e8399bf9dbb03f1c4970d16
Allan Weber <a.cassianoweber@gmail.com>
2020-09-14 18:35:50 +0200

    Remove unused code

--- src/main/java/com/allanweber/candidatesprocesses/candidate/dto/GitHubProfileMessage.java
+++ src/main/java/com/allanweber/candidatesprocesses/candidate/dto/GitHubProfileMessage.java
@@ -1,12 +1,10 @@
 package com.allanweber.candidatesprocesses.candidate.dto;
 
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Data
-@JsonPropertyOrder({ "candidateId", "apiProfile", "token" })
 public class GitHubProfileMessage {
     private String candidateId;
 
